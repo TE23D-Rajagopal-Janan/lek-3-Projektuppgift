a9fadd38ab897ef85dfde9ceac239b4e0a0a2dd2
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2025-09-16 13:05:30 +0200

    Dag 2

--- src/App.java
+++ src/App.java
@@ -5,22 +5,30 @@ public class App {
         Scanner input = new Scanner (System.in);
         int[] bokningar = new int[20];
         double pris = 299.90; 
-        
-        System.out.println("1. Vill du boka en plats");
-        System.out.println("2. Antal lediga platser");
-        System.out.println("3. Stäng ner (Avsluta)");
-        int val = input.nextInt();   
-
-        if (val == 1) {
-            System.out.println("Ange personnummer");
-            int personnummer = input.nextInt(); 
-            bokningar[0] = personnummer;
-            System.out.println("Nu är personummer" + personnummer + "Registerat");
-        }
-        else if (val == 2) {
-            for (int i = 0; i < bokningar.length; i++) {
-                System.out.println(bokningar[i]);
+        boolean meny=true;
+            while (meny) {
+            
+            System.out.println("1. Vill du boka en plats");
+            System.out.println("2. Antal lediga platser");
+            System.out.println("3. Stäng ner (Avsluta)");
+            int val = input.nextInt();   
+            
+            if (val == 1) {
+                System.out.println("Ange personnummer");
+                int personnummer = input.nextInt(); 
+                bokningar[0] = personnummer;
+                System.out.println("\n" +"Nu är personummer:" + personnummer + " Registerat");
+                System.out.println("\n" +"Du har betalat " + pris + "\n");
             }
+            else if (val == 2) {
+                for (int i = 0; i < bokningar.length; i++) {
+                    System.out.println(bokningar[i]);
+                }
+            }
+            else if (val == 3){
+                System.out.println("Du har avslutat");
+                meny = false;
+                }
         }
 
         
